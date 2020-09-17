65b2a7c284371f8ce9413555802d86d6591ff034
Marcos <sierramarcos146@gmail.com>
2020-09-17 11:00:25 -0600

    proyectoBase

---
+++ ManejoCafe/src/main/java/org/kurokami/ManejoCafe/ManejoCafeApplication.java
@@ -0,0 +1,13 @@
+package org.kurokami.ManejoCafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManejoCafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManejoCafeApplication.class, args);
+	}
+
+}
