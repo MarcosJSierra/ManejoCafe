55ec223c8e1d67cb17c7fe3ccaaeb1cb202b9d2f
Marcos <sierramarcos146@gmail.com>
2020-09-24 13:02:12 -0600

    subiendo configuraciones

---
+++ ManejoCafe/src/main/java/org/kurokami/ManejoCafe/configuraciones/WebConfig.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.kurokami.ManejoCafe.configuraciones;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ *
+ * @author marcos
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer{
+    
+    @Override
+    public void addViewControllers(ViewControllerRegistry registro){
+        
+        registro.addViewController("/sec/login");
+    }
+}
