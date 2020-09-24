0f3dd4dfc388179aba889b76821d72edd820cbb9
Marcos <sierramarcos146@gmail.com>
2020-09-24 11:24:09 -0600

    subiendo cambios

--- ManejoCafe/src/main/java/org/kurokami/ManejoCafe/controller/MainSiteController.java
+++ ManejoCafe/src/main/java/org/kurokami/ManejoCafe/controller/MainSiteController.java
@@ -5,7 +5,11 @@
  */
 package org.kurokami.ManejoCafe.controller;
 
+import java.util.ArrayList;
+import lombok.extern.slf4j.Slf4j;
 import org.kurokami.ManejoCafe.Modelo.Cliente;
+import org.kurokami.ManejoCafe.Modelo.Puesto;
+import org.kurokami.ManejoCafe.Modelo.Usuario;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
@@ -18,6 +22,7 @@ import org.springframework.web.bind.annotation.PostMapping;
  * @author marcos
  */
 @Controller
+@Slf4j
 public class MainSiteController {
     
     @GetMapping("/")
@@ -35,4 +40,18 @@ public class MainSiteController {
         return "redirect:/";
     }
     
+    @GetMapping("/usuario/registro")
+    public String registrarUsuario(Usuario usuario, Model model){
+        model.addAttribute("usuario", usuario);
+        var rol = new Puesto();
+        model.addAttribute("rol", rol);
+        return "mainsite/registroUsuario";
+    }
+    
+    @PostMapping("/usuario/registrar")
+    public String guardarRegistroUsuario(Cliente cliente, Puesto rol){
+        log.info(cliente.toString());
+        log.info(rol.toString());
+        return "redirect:/";
+    }
 }
